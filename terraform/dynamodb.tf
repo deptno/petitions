841e90093c6f4c22002ea008817130e032901405
bglee <deptno@gmail.com>
2020-01-25 15:21:29 +0900

    feat: terraform

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,27 @@
+resource aws_dynamodb_table aa {
+  name = "${terraform.workspace}-petitions"
+  hash_key = "hk"
+  range_key = "rk"
+  billing_mode = "PAY_PER_REQUEST"
+
+  global_secondary_index {
+    name = "rkT"
+    hash_key = "rk"
+    range_key = "t"
+    projection_type = "ALL"
+  }
+
+  ttl {
+    attribute_name = "ttl"
+    enabled        = true
+  }
+
+  attribute {
+    name = "hk"
+    type = "S"
+  }
+  attribute {
+    name = "rk"
+    type = "S"
+  }
+}
