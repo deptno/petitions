24551c6c1001b0d1cad30efba4e8558a50e75a35
bglee <deptno@gmail.com>
2020-01-27 22:21:34 +0900

    fix: change build config

--- packages/ptt-datalake/src/constant.ts
+++ packages/ptt-datalake/src/constant.ts
@@ -1,4 +1,3 @@
-export const const_min_5 = 60 * 5
 export const const_url = 'https://www1.president.go.kr'
 export const const_best_url = `${const_url}/petitions/best`
 export const const_yyyyMMDdTHHMmxx = 'yyyy-MM-dd\'T\'HH:mmxx'
