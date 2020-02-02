ec422860fbbcee41b472abb193da3d29e5c68834
bglee <deptno@gmail.com>
2020-02-02 19:24:04 +0900

    feat: deploy backend

--- packages/ptt_backend/constant.ts
+++ packages/ptt_backend/constant.ts
@@ -1,7 +1,3 @@
-import {scryptSync} from "crypto"
-
 export const const_is_production = process.env.STAGE === 'prd'
-export const const_stage = process.env.STAGE!
 export const const_cors = process.env.CORS!
 export const const_ddb_table = process.env.PTT_DDB_TABLE!
-export const const_salt = scryptSync('salt text', 'salt', 24);
