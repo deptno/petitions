8f7e80cd470eabd2031986a8aee4e61c5257672c
deptno <deptno@gmail.com>
2020-02-11 11:38:15 +0900

    feat: add archive cron

--- packages/ptt_datalake/src/constant.ts
+++ packages/ptt_datalake/src/constant.ts
@@ -2,3 +2,4 @@ export const const_url = 'https://www1.president.go.kr'
 export const const_best_url = `${const_url}/petitions/best`
 export const const_yyyyMMDdTHHMmxx = 'yyyy-MM-dd\'T\'HH:mmxx'
 export const const_ddb_table = process.env.PTT_DDB_TABLE!
+export const const_s3 = process.env.PTT_S3!
